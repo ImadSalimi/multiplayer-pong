0d2b939700fc37bc3b70a9009f09149a995bc80a
ImadSalimi <salimi.imad@gmail.com>
2015-12-26 03:39:52 +0000

    Made src folder root for project

---
+++ multiplayer/pong/game/Pong.java
@@ -0,0 +1,31 @@
+package multiplayer.pong.game;
+
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Pong extends JFrame {
+	private final static int W_WIDTH = 800, W_HEIGHT = 600;
+	private PongPanel panel;
+	
+	public Pong() {
+		setSize(W_WIDTH, W_HEIGHT);
+		setTitle("Pong Online");
+		setBackground(Color.white);
+		setResizable(false);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public void start() {
+		panel = new PongPanel(this);
+		add(panel);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Pong game = new Pong();
+		game.start();
+	}
+
+}
